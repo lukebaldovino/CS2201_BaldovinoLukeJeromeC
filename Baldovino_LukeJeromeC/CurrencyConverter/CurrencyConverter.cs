2ae89d27083b8f88c468996f851585aea67d1735
BALDOVINO, LUKE JEROME C. <lukejeromebaldovino@gmail.com>
2026-02-10 09:41:06 +0800

    finished

---
+++ Baldovino_LukeJeromeC/CurrencyConverter/CurrencyConverter.cs
@@ -0,0 +1,12 @@
+﻿// See https://aka.ms/new-console-template for more information
+double usdAmount, exchangeRate, convertedAmount;
+
+Console.Write("Enter amount in USD: ");
+usdAmount = Convert.ToDouble(Console.ReadLine());
+
+Console.Write("Enter exchange rate From USD to EUR: ");
+exchangeRate = Convert.ToDouble(Console.ReadLine());
+
+convertedAmount = usdAmount * exchangeRate;
+
+Console.WriteLine($"Amount in EUR: {convertedAmount:N2}");
