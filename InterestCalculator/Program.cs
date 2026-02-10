e835ced96d9be8d9e389f9a3462136e7c12d1a3b
BALDOVINO, LUKE JEROME C. <lukejeromebaldovino@gmail.com>
2026-02-10 09:16:44 +0800

    init commit

---
+++ InterestCalculator/Program.cs
@@ -0,0 +1,15 @@
+﻿// See https://aka.ms/new-console-template for more information
+double principal, rate, time, simpleInterest;
+
+Console.Write("Enter principal amount: ");
+principal = Convert.ToDouble(Console.ReadLine());
+
+Console.Write("Enter rate of interest: ");
+rate = Convert.ToDouble(Console.ReadLine());
+
+Console.Write("Enter time in years: ");
+time = Convert.ToDouble(Console.ReadLine());
+
+simpleInterest = (principal * rate * time) / 100;
+
+Console.WriteLine($"Simple Interest: {simpleInterest:N2}");
